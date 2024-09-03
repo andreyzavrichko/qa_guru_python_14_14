391b4f1b3d329b2a6c7befe47a3b556edf862357
Andrey <andrey@zavrichko.dev>
2024-09-03 20:22:28 +0300

    finish

--- tests/test_search.py
+++ tests/test_search.py
@@ -21,7 +21,7 @@ def test_search_visible(setup_browser):
 def test_search_count(setup_browser):
     search_page.open()
     search_page.type_search_input("ролики")
-    search_page.check_result_count("Найден 41 товар в 8 категориях")
+    search_page.check_result_count("Найдено 29 товаров в 5 категориях")
 
 
 @allure.tag("web")
